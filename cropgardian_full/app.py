71c8baaad8ed6d18906b64c7093c8dbda3546d96
ponthangaiah-dotcom <ponthangaiah@gmail.com>
2026-05-22 12:33:10 +0530

    Update app.py

--- cropgardian_full/app.py
+++ cropgardian_full/app.py
@@ -461,7 +461,8 @@ with app.app_context():
     db.create_all()
     load_model()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
